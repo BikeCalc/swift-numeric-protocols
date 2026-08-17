87ad2eae9a6d892098599e72f37f8341b542db93
Alexandre H. Saad <alexandresaad@icloud.com>
2026-08-17 22:13:27 +0100

    refactor: internalize fraction transformation protocols

---
+++ Sources/ExperimentalNumericTypes/Fraction/Normalizable.swift
@@ -0,0 +1,26 @@
+// This source file is part of the Numerics Extended open source project
+//
+// Copyright (c) 2021-2026 A. H. de Quatre Ltd. and the Numerics Extended project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.md for license information
+// See CONTRIBUTORS.txt for the list of Numerics Extended project authors
+
+/// A type that can be converted to a normalized equivalent representation.
+internal protocol Normalizable {
+    /// A boolean value indicating whether this value can be normalized.
+    var isNormalizable: Bool { get }
+
+    /// A boolean value indicating whether this value is normalized.
+    var isNormalized: Bool { get }
+
+    /// Returns a normalized equivalent representation of this value.
+    func normalized() -> Self
+}
+
+extension Normalizable {
+    /// Replaces this value with its normalized representation.
+    internal mutating func normalize() {
+        self = self.normalized()
+    }
+}
---
+++ Sources/ExperimentalNumericTypes/Fraction/Simplifiable.swift
@@ -0,0 +1,28 @@
+// This source file is part of the Numerics Extended open source project
+//
+// Copyright (c) 2021-2026 A. H. de Quatre Ltd. and the Numerics Extended project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.md for license information
+// See CONTRIBUTORS.txt for the list of Numerics Extended project authors
+
+/// A type that can be reduced to a simpler equivalent representation.
+internal protocol Simplifiable {
+    /// A boolean value indicating whether this value can be simplified further.
+    var isSimplifiable: Bool { get }
+
+    /// Returns a simpler equivalent representation of this value.
+    func simplified() -> Self
+}
+
+extension Simplifiable {
+    /// A boolean value indicating whether this value is already simplified.
+    internal var isSimplified: Bool {
+        return self.isSimplifiable == false
+    }
+
+    /// Replaces this value with its simplified representation.
+    internal mutating func simplify() {
+        self = self.simplified()
+    }
+}
